26d28124a3cd49595bf35d29b58f792e118c46aa
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-10-07 16:06:52 -0400

    Initial Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  
+  render() { 
+    return ( 
+      <div className="container-fluid">
+
+      </div>
+
+    );
+  }
+}
+ 
+export default App;
+
+
