ebe9ed89adab88894f5106a0bb449a407dbf9562
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-10-07 17:21:49 -0400

    Created function to get all songs
    - Imported Song Table component.
    - Added Table Headers to this.state.
    - Called getAllSongs in CDM.
    - Created function to get all songs and update state.songs.
    - Added SongTable to render and passed in props.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
 import './App.css';
 import React, { Component } from 'react';
+import axios from 'axios';
+import SongTable from './SongTable/SongTable';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = { 
+      tableHeads: ['Genre', 'Artist', 'Song Title', 'Album', 'Release Date', 'Likes', 'Dislikes'],
+      songs: [],
+    }
+
+
+  }
+
+  componentDidMount() {
+    this.getAllSongs()
+  }
+
+  async getAllSongs(){
+    let response = await axios.get('http://127.0.0.1:8000/music/')
+    this.setState({
+      songs: response.data
+    })
   }
   
   render() { 
     return ( 
       <div className="container-fluid">
-
+        <SongTable songs={this.state.songs} headers={this.state.tableHeads}/>
       </div>
 
     );
