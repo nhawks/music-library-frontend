8cfe96084e6469a64ed972c92311fc33dbc1020f
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-10-12 13:17:51 -0400

    Added styling to Accordian

--- src/components/AddSongForm/AddSongForm.jsx
+++ src/components/AddSongForm/AddSongForm.jsx
@@ -30,25 +30,29 @@ class AddSongForm extends Component {
             <div className="container">
             <Accordion>
                 <Accordion.Item eventKey="0">
-                    <Accordion.Header>Accordion Item #1</Accordion.Header>
+                    <Accordion.Header>Add Song to Library</Accordion.Header>
                         <Accordion.Body>
-                        <form onSubmit={this.handleSubmit}>
-                                <label> Genre 
-                                    <input name="genre" onChange={this.handleChange} value={this.state.genre} />
-                                </label>
-                                <label> Artist 
-                                    <input name="artist" onChange={this.handleChange} value={this.state.artist} />
-                                </label>
-                                <label> Song Title 
-                                    <input name="title" onChange={this.handleChange} value={this.state.title} />
-                                </label>
-                                <label> Song Album 
-                                    <input name="album" onChange={this.handleChange} value={this.state.album} />
-                                </label>
-                                <label> Release Date
-                                    <input name="release_date" onChange={this.handleChange} value={this.state.release_date} />
-                                </label>
-                                <input type="Submit" value="Add Song" />
+                            <form onSubmit={this.handleSubmit} className="row row-cols-lg-auto g-3 align-items-center">
+                                <div class="row g-3">
+                                    <div class="col">
+                                        <input type="text" class="form-control" placeholder="Genre" aria-label="Genre" name="genre" placeholder="Genre" onChange={this.handleChange} value={this.state.genre} />
+                                    </div>
+                                    <div class="col">
+                                        <input type="text" class="form-control" placeholder="Artist" aria-label="Artist" name="artist" onChange={this.handleChange} value={this.state.artist}/>
+                                    </div>
+                                    <div class="col">
+                                        <input type="text" class="form-control" placeholder="Song Title" aria-label="Song Title" name="title" onChange={this.handleChange} value={this.state.title}/>
+                                    </div>
+                                    <div class="col">
+                                        <input type="text" class="form-control" placeholder="Album" aria-label="Album" name="album" onChange={this.handleChange} value={this.state.album}/>
+                                    </div>
+                                    <div class="col">
+                                        <input type="text" class="form-control" placeholder="Release Date" aria-label="Release Date" name="release_date" onChange={this.handleChange} value={this.state.release_date}/>
+                                    </div>
+                                </div>
+                                    <div className="row g-3">
+                                        <button type="submit" className="btn btn-primary">Add Song</button>
+                                    </div>
                             </form>
                         </Accordion.Body>
                 </Accordion.Item>
