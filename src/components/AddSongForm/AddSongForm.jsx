de322a56d39315fadf39e4447bbfa7be7945b8a3
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-10-12 17:37:59 -0400

    Adding styling to form/accordion.

--- src/components/AddSongForm/AddSongForm.jsx
+++ src/components/AddSongForm/AddSongForm.jsx
@@ -28,26 +28,28 @@ class AddSongForm extends Component {
     render() { 
         return ( 
             <div className="container">
-            <Accordion>
-                <Accordion.Item eventKey="0">
-                    <Accordion.Header>Add Song to Library</Accordion.Header>
-                        <Accordion.Body>
+            <Accordion className="accordion">
+                <Accordion.Item eventKey="0" className="accordion-item">
+                    <Accordion.Header className="accordion-header">
+                        <p className="text-dark text-center">Add Song to Library</p>
+                    </Accordion.Header>
+                        <Accordion.Body className="accordion-body">
                             <form onSubmit={this.handleSubmit} className="row row-cols-lg-auto g-3 align-items-center">
                                 <div class="row g-3">
                                     <div class="col">
-                                        <input type="text" class="form-control" placeholder="Genre" aria-label="Genre" name="genre" placeholder="Genre" onChange={this.handleChange} value={this.state.genre} />
+                                        <input type="text" class="form-control bg-light text-primary" placeholder="Genre" aria-label="Genre" name="genre" placeholder="Genre" onChange={this.handleChange} value={this.state.genre} />
                                     </div>
                                     <div class="col">
-                                        <input type="text" class="form-control" placeholder="Artist" aria-label="Artist" name="artist" onChange={this.handleChange} value={this.state.artist}/>
+                                        <input type="text" class="form-control bg-light text-primary" placeholder="Artist" aria-label="Artist" name="artist" onChange={this.handleChange} value={this.state.artist}/>
                                     </div>
                                     <div class="col">
-                                        <input type="text" class="form-control" placeholder="Song Title" aria-label="Song Title" name="title" onChange={this.handleChange} value={this.state.title}/>
+                                        <input type="text" class="form-control bg-light text-primary" placeholder="Song Title" aria-label="Song Title" name="title" onChange={this.handleChange} value={this.state.title}/>
                                     </div>
                                     <div class="col">
-                                        <input type="text" class="form-control" placeholder="Album" aria-label="Album" name="album" onChange={this.handleChange} value={this.state.album}/>
+                                        <input type="text" class="form-control bg-light text-primary" placeholder="Album" aria-label="Album" name="album" onChange={this.handleChange} value={this.state.album}/>
                                     </div>
                                     <div class="col">
-                                        <input type="text" class="form-control" placeholder="Release Date" aria-label="Release Date" name="release_date" onChange={this.handleChange} value={this.state.release_date}/>
+                                        <input type="text" class="form-control bg-light text-primary" placeholder="Release Date" aria-label="Release Date" name="release_date" onChange={this.handleChange} value={this.state.release_date}/>
                                     </div>
                                 </div>
                                     <div className="row g-3">
@@ -65,42 +67,3 @@ class AddSongForm extends Component {
 }
  
 export default AddSongForm;
-
-
-
-{/* <div className="accordion" id="accordionExample">
-                <div className="accordion-item">
-                    <h2 className="accordion-header" id="headingOne">
-                        <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                            Add Song
-                        </button>
-                    </h2>
-                    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample" style="">
-                        <div className="accordion-body">
-                            <form onSubmit={this.handleSubmit}>
-                                <label> Genre 
-                                    <input name="genre" onChange={this.handleChange} value={this.state.genre} />
-                                </label>
-                                <br />
-                                <label> Artist 
-                                    <input name="artist" onChange={this.handleChange} value={this.state.artist} />
-                                </label>
-                                <br />
-                                <label> Song Title 
-                                    <input name="title" onChange={this.handleChange} value={this.state.title} />
-                                </label>
-                                <br />
-                                <label> Song Album 
-                                    <input name="album" onChange={this.handleChange} value={this.state.album} />
-                                </label>
-                                <br />
-                                <label> Release Date
-                                    <input name="release_date" onChange={this.handleChange} value={this.state.release_date} />
-                                </label>
-                                <br />
-                                
-                                <input type="Submit" value="Add Song" />
-                            </form>
-                        </div>
-                </div>
-            </div> */}
